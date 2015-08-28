681fdb9fe7c7d434d57b7fd3a4e97137c686be33
meganemura <meganemura@users.noreply.github.com>
2015-08-29 00:38:37 +0900

    Remove lines

--- client/app.js
+++
@@ -1,8 +0,0 @@
-var myApplication = {               // (1)
-  controller: function() {},        // (2)
-  view: function(controller) {      // (3)
-    return m("h1", "hello world");  // (4)
-  }
-};
-
-m.mount(document.getElementById("root"), myApplication);  // (5)
