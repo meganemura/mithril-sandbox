8cff7fe1bc208096723db4aa0baa597174907f3c
meganemura <meganemura@users.noreply.github.com>
2015-08-29 13:10:23 +0900

    Use m.trust

--- client/xss.js
+++ client/xss.js
@@ -2,5 +2,5 @@ var m = require('./mithril.js');
 var render = require('mithril-node-render');
 
 console.log(render(
-      m("div", "<button onclick=\"alert('hello');\">test</button>")
+      m("div", m.trust("<button onclick=\"alert('hello');\">test</button>"))
 ));
