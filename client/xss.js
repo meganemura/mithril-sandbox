dd522c3fc7b2fb6cafbfdaa1d07795feb1d61c39
meganemura <meganemura@users.noreply.github.com>
2015-08-30 08:50:03 +0900

    Remove client/xss.js

--- client/xss.js
+++
@@ -1,6 +0,0 @@
-var m = require('./mithril.js');
-var render = require('mithril-node-render');
-
-console.log(render(
-      m("div", m.trust("<button onclick=\"alert('hello');\">test</button>"))
-));
