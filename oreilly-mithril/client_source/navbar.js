b18e78a6adde8e429b4d1a5d2e39f226da049f77
meganemura <meganemura@users.noreply.github.com>
2015-08-30 12:30:19 +0900

    NavBar: Use m.route() for url

--- oreilly-mithril/client_source/navbar.js
+++ oreilly-mithril/client_source/navbar.js
@@ -2,6 +2,8 @@ var m = require("mithril");
 
 var NavBar = {
   view: function(ctrl, args) {
+    var activeUrl = m.route();
+
     return  m(".container", [
         m(".navbar-header", [
           m("button.navbar-toggle.collapsed[aria-controls='navbar'][aria-expanded='false'][data-target='#navbar'][data-toggle='collapse'][type='button']", [
@@ -14,7 +16,9 @@ var NavBar = {
         ]),
         m(".collapse.navbar-collapse[id='navbar']", [
           m("ul.nav.navbar-nav", [
-            m("li.active", [m("a[href='#']", "Home")]),
+            m("li", {class: args.root === activeUrl ? "active" : ""}, [
+              m("a", {href: args.root, config: m.route}, args.pages[args.root])
+            ]),
             m("li", [m("a[href='#about']", "About")]),
             m("li", [m("a[href='#contact']", "Contact")])
           ])
